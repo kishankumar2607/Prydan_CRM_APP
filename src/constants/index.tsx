31ced0223562d09063bef65e644ca4626468747b
Kishan Kumar Das <88204543+kishan-prydan@users.noreply.github.com>
2022-07-06 17:35:21 +0530

    delete

--- src/constants/index.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * @description export constant for the hole application
- */
-export { default as colors } from "./colors";
-export { default as I18n } from "./I18n";
-export { default as font } from "./font";
