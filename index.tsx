6a8de0a91ddcaed17c91c56684a93dd3fecb1de8
kishankumar-prydan <88204543+kishan-prydan@users.noreply.github.com>
2022-07-07 08:24:42 +0530

    added app.txt file to the project

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
-import 'react-native-gesture-handler';
-import { enableScreens } from 'react-native-screens';
-import { AppRegistry } from 'react-native';
-import App from './src/index';
-import { name as appName } from './app.json';
+import "react-native-gesture-handler";
+import { enableScreens } from "react-native-screens";
+import { AppRegistry } from "react-native";
+// import App from './src/index';
+import App from "./App";
+import { name as appName } from "./app.json";
 enableScreens();
 AppRegistry.registerComponent(appName, () => App);
